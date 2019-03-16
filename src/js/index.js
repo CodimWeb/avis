11ed5e411116d0337a8a962102c641613a65083d
Jk <ftforweb@gmail.com>
2019-03-16 11:19:02 +0300

    fix slick

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,18 @@
 import $ from 'jquery';
 import '../scss/style.scss';
 import bootstrap from 'bootstrap';
+import '../js/slick.min.js';
 
+
+$(".slider").not('.slick-initialized').slick({
+    
+});
+
+// $('.slider').slick({
+//     // dots: true,
+//     // infinite: true,
+//     // speed: 300,
+//     slidesToShow: 4,
+//     // centerMode: true,
+//     // variableWidth: true
+//   });
